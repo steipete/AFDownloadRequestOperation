7fdb02176e62fd23d1dec7e2e6bba95f132f61f5
Stefan Paychère <stefan.paychere@epyx.ch>
2013-12-27 15:47:43 +0100

    Added support for OS X
    - Fixed OS X warning on size_t to uint32_t conversion
    - Modified podspec file to support same platforms as AFNetworking
    
    Signed-off-by: Stefan Paychère <stefan.paychere@epyx.ch>

--- AFDownloadRequestOperation.m
+++ AFDownloadRequestOperation.m
@@ -319,7 +319,7 @@ typedef void (^AFURLConnectionProgressiveOperationProgressBlock)(AFDownloadReque
 + (NSString *)md5StringForString:(NSString *)string {
     const char *str = [string UTF8String];
     unsigned char r[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(str, strlen(str), r);
+    CC_MD5(str, (uint32_t)strlen(str), r);
     return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
             r[0], r[1], r[2], r[3], r[4], r[5], r[6], r[7], r[8], r[9], r[10], r[11], r[12], r[13], r[14], r[15]];
 }
--- AFDownloadRequestOperation.podspec
+++ AFDownloadRequestOperation.podspec
@@ -5,7 +5,8 @@ Pod::Spec.new do |s|
   s.homepage       = "https://github.com/steipete/AFDownloadRequestOperation"
   s.author         = { 'Peter Steinberger' => 'steipete@gmail.com' }
   s.source         = { :git => 'https://github.com/steipete/AFDownloadRequestOperation.git', :tag => s.version.to_s }
-  s.platform       = :ios, '6.0'
+  s.ios.deployment_target = '6.0'
+  s.osx.deployment_target = '10.8'
   s.requires_arc   = true
   s.source_files   = '*.{h,m}'
   s.license        = 'MIT'
